9ba7899c1d64c3bfe7f109010bdc80eb88fc6d6d
abderrahmanebennaceur <abderrahmane.bennaceur.213@gmail.com>
2024-12-29 20:42:13 +0100

    feat(app-setup): import and call components in App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import CodeEditorFeature from './components/sections/CodeEditorFeature'
+import CollaborationFeature from './components/sections/CollaborationFeature'
+import EasyToUse from './components/sections/EasyToUse'
+import FeaturedDevelopers from './components/sections/FeaturedDevelopers'
+import GrowingMetabox from './components/sections/GrowingMetabox'
 import Hero from './components/sections/Hero'
+import OurPlatform from './components/sections/OurPlatform'
+import Showcase from './components/sections/Showcase'
+import StartCoding from './components/sections/StartCoding'
+import Testimonial from './components/sections/Testimonial'
+import Trial from './components/sections/Trial'
+import TrustedBy from './components/sections/TrustedBy'
 
 const App = () => {
   return (
     <div className="min-h-screen w-full dark:bg-black bg-black  dark:bg-dot-white/[0.2] bg-dot-white/[0.2] relative flex flex-col items-center justify-center">
-      {/* Radial gradient for the container to give a faded look */}
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <Navbar />
       <Hero />
+      <TrustedBy />
+      <Showcase />
+      <CodeEditorFeature />
+      <OurPlatform />
+      <CollaborationFeature />
+      <EasyToUse />
+      <FeaturedDevelopers />
+      <StartCoding />
+      <GrowingMetabox />
+      <Testimonial />
+      <Trial />
+      <Footer />
     </div>
   )
 }
